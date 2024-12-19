9da7196bc35ed42f014eade5e1dde353bc0bb9d9
Pedro Batista <pedroplpellegrinni1918@gmail.com>
2024-12-18 21:39:43 -0300

    commit full

---
+++ scripts-Python/1º exercicio Python.py
@@ -0,0 +1,3 @@
+print('1ª) crie um script python que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o nome digitado')
+nome = input('óla, qual seu nome?')
+print('seja bem vindo(a) ' , nome)
---
+++ scripts-Python/2º exercicio Python.py
@@ -0,0 +1,7 @@
+print('2ª) Crie um script python que leia o dia o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.')
+
+print('Óla, diga sua data de nascimento')
+dia = input('dia?')
+mes = input('mês?')
+ano = input('ano?')
+print('você nasceu em ' ,dia,'/',mes,'/',ano)
